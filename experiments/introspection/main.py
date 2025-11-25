bb748ac6d88d38c4937aeef61eece85d48dfaae3
Julian Schulz <wuschelschulz8@gmail.com>
2025-11-25 19:40:45 +0000

    pull debug resutls

--- experiments/introspection/main.py
+++ experiments/introspection/main.py
@@ -7,15 +7,17 @@ from research_scaffold.argparsing import get_base_argparser, process_base_args
 from functions.compute_steering_vectors import compute_steering_vectors
 from functions.introspection_eval import introspection_eval
 
+function_map = {
+    "compute_steering_vectors": compute_steering_vectors,
+    "introspection_eval": introspection_eval,
+}
+
 if __name__ == "__main__":
     args = get_base_argparser().parse_args()
     config_path, meta_config_path, sweep_config_path = process_base_args(args)
     
     execute_experiments(
-        function_map={
-            "compute_steering_vectors": compute_steering_vectors,
-            "introspection_eval": introspection_eval,
-        },
+        function_map=function_map,
         config_path=config_path,
         meta_config_path=meta_config_path,
         sweep_config_path=sweep_config_path,
